5582df22996be8f68a434666f25860fbbdcaadca
ahmedzain958 <ahmedzain958@gmail.com>
2019-03-13 08:50:00 -0700

    add nav component

--- app/src/main/java/com/hogp/testingeverything/MainActivity.java
+++ app/src/main/java/com/hogp/testingeverything/MainActivity.java
@@ -10,13 +10,11 @@ import javax.inject.Inject;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    @Inject
-    Car car;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
     }
 }
